bebf11d991b5e16644226df342a7b2dbdb861bd0
Venkata Lohith <venkatalohith25@gmail.com>
2026-07-05 15:43:58 +0530

    Added Language Basics - Command Line Arguments programs

---
+++ 01_Language_Basics/A1.java
@@ -0,0 +1,5 @@
+public class A1 {
+    public static void main(String[] args) {
+        System.out.println(args[0]+" Technologies "+args[1]);
+    }
+}
---
+++ 01_Language_Basics/A3.java
@@ -0,0 +1,7 @@
+public class A3 {
+    public static void main(String[] args) {
+        int a1=Integer.parseInt(args[0]);
+        int a2=Integer.parseInt(args[1]);
+        System.out.println("The sum of "+a1+" and "+a2+" is: "+(a1+a2));
+    }
+}

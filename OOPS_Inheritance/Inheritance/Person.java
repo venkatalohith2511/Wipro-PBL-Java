668dab8bb4b4980b06a0cf4529ac2cebceab0e70
Venkata Lohith <venkatalohith25@gmail.com>
2026-07-09 11:20:36 +0530

    Added Inheritance

---
+++ OOPS_Inheritance/Inheritance/Person.java
@@ -0,0 +1,15 @@
+class Person {
+    private String name;
+    
+    Person(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
